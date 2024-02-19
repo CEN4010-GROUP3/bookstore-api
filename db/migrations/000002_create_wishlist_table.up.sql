ac1d9953921da12eeddd77108421ec336c75413e
David Chico <64663174+PikaSaiyanZero@users.noreply.github.com>
2024-02-18 20:07:54 -0500

    Updated foreign key code.

--- db/migrations/000002_create_wishlist_table.up.sql
+++ db/migrations/000002_create_wishlist_table.up.sql
@@ -1,4 +1,6 @@
 create table public.wishlist (
     book_isbn varchar(14),
-    user_id int -- User database structure to be confirmed with other teammates.
+    user_id int, -- User database structure to be confirmed with other teammates.
+    foreign key (book_isbn) references public.books (isbn)
+    -- foreign key to users table, to be set at that time
 );
