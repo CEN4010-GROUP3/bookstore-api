daa15db8df8d4d6a21aee5a41b1a9cd4be685b2a
David Chico <64663174+PikaSaiyanZero@users.noreply.github.com>
2024-03-07 10:49:48 -0500

    Basic Wishlist class

---
+++ src/main/java/edu/fiu/cen4010/group3/model/Wishlist.java
@@ -0,0 +1,54 @@
+package edu.fiu.cen4010.group3.model;
+
+import java.util.ArrayList;
+
+public class Wishlist {
+    private String name;
+    private int wishlistId;
+    private int userId;
+    private ArrayList<Book> contents;
+
+    public Wishlist() {
+    }
+
+    // Constructor without contents.
+    public Wishlist(String name, int userId, int wishlistId) {
+        this.name = name;
+        this.userId = userId;
+        this.wishlistId = wishlistId;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getWishlistId() {
+        return wishlistId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public ArrayList<Book> getContents() {
+        return contents;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWishlistId(int wishlistId) {
+        this.wishlistId = wishlistId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setContents(ArrayList<Book> contents) {
+        this.contents = contents;
+    }
+}
