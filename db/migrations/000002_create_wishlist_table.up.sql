66c12d84993c440e23c5446bb1499da2d1aa51a0
David Chico <64663174+PikaSaiyanZero@users.noreply.github.com>
2024-03-06 21:05:47 -0500

    Changed number on the wishlist commit to have user made first.

--- db/migrations/000002_create_wishlist_table.up.sql
+++
@@ -1,6 +0,0 @@
-create table public.wishlist (
-    book_isbn varchar(14),
-    user_id int, -- User database structure to be confirmed with other teammates.
-    foreign key (book_isbn) references public.books (isbn)
-    -- foreign key to users table, to be set at that time
-);
